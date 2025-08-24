97ec266be08d5f435a8591099a41d1c8bb8dc482
Suraj Ingale <svingale2001@gmail.com>
2025-08-24 19:02:34 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
+  base: '/Enphoria-Coffee-React/', // Replace with your repository name
   optimizeDeps: {
     exclude: ['lucide-react'],
-  },
-  css: {
-    postcss: './postcss.config.cjs', // Only if you rename to .cjs
   },
 });
